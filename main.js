9d37209138feabbb6d03558def99fdeba986552b
Jack5079 <Jack5079>
2019-08-05 15:36:29 -0400

    add irony

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
-var v = document.getElementById("count"); // The V-Bucks count
-var u = document.getElementById("user"); // The username input
+/* global swal */
+var v = document.getElementById('count') // The V-Bucks count
+var u = document.getElementById('user') // The username input
 // Submit code
-function submit() {
-    if (v.value == "" || u.value == "") { // If either of the values aren't filled
-        swal({ // Show an alert to the user
-            title: "Oops!",
-            text: "Fill in the username and V-Bucks count.",
-            icon: "error",
-        });
-    } else if (parseInt(v.value) <= 0) { // If the V-Bucks number is negative
-        swal({ // Show an alert to the user
-            title: "Oops!",
-            text: "Use a postive number for the V-Bucks count.",
-            icon: "error",
-        });
-    } else { // When the user types in the info, gnome them.
-        window.location.href = "https://www.youtube.com/embed/6n3pFFPSlW4?autoplay=1&controls=0"; // Autoplaying YT embed link without controls
-    }
-}
+document.querySelector('button').addEventListener('click', function () {
+  if (v.value === '' || u.value === '') { // If either of the values aren't filled
+    swal({ // Show an alert to the user
+      title: 'tf...',
+      text: 'fill in the form retard',
+      icon: 'error'
+    })
+  } else if (parseInt(v.value) <= 0) { // If the V-Bucks number is negative
+    swal({ // Show an alert to the user
+      title: 'Oops!',
+      text: 'we can\'t take away vbucks \n this isn\'t funny if you are taking away someone else\'s vbucks',
+      icon: 'error'
+    })
+  } else { // When the user types in the info, gnome them.
+    window.location.href = 'https://www.youtube.com/embed/6n3pFFPSlW4?autoplay=1&controls=0' // Autoplaying YT embed link without controls
+  }
+})
